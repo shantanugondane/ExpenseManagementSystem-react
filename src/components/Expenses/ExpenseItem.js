5be69a8718d2c413aa876cc0d1e2b8cb69b625f2
Shantanu Gondane <113772055+shantanugondane@users.noreply.github.com>
2022-12-14 02:37:20 +0530

    expense page

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  let title = props.title;
+
+
+  const clickHandler = () => {
+    title = 'Updated!';
+    console.log(title);
+  }
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change tiitle</button> 
+    </Card>
+  );
+}
+
+export default ExpenseItem;
